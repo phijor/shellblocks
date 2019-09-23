d895962c27e5e2182f3433b2082d90271a1791b4
Philipp Joram <mail@phijor.me>
2019-09-23 13:24:07 +0200

    Parse blocks to display from command line

--- src/main.rs
+++ src/main.rs
@@ -16,33 +16,45 @@ use crate::user_at_host::UserAtHost;
 use std::env;
 use std::ops::Deref;
 
+struct Blocks(Vec<Block>);
+
+impl Blocks {
+    pub fn from_names<I: Iterator<Item = String>>(names: I) -> Self {
+        let mut blocks = Vec::new();
+        let cwd = env::current_dir().unwrap_or_else(|_| ".".into());
+
+        for name in names {
+            let block: Option<Block> = match name.as_str() {
+                "user-at-host" => UserAtHost::default().get_block(),
+                "cwd" => Cwd::new(&cwd).get_block(),
+                "git" => Git::new(&cwd).get_block(),
+                "pipenv" => Pipenv::default().get_block(),
+                _ => {
+                    eprintln!("Unknown source {}", name);
+                    continue;
+                }
+            };
+
+            match block {
+                Some(block) => blocks.push(block),
+                None => {}
+            }
+        }
+
+        Self(blocks)
+    }
+}
+
+impl Deref for Blocks {
+    type Target = Vec<Block>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
-    struct Blocks(Vec<Block>);
-
-    impl Blocks {
-        pub fn add_from<S: Source>(&mut self, source: S) {
-            source.get_block().map(|block| self.0.push(block));
-        }
-    }
-
-    impl Deref for Blocks {
-        type Target = Vec<Block>;
-
-        fn deref(&self) -> &Self::Target {
-            &self.0
-        }
-    }
-
-    let mut blocks = Blocks(Vec::new());
-
-    let cwd = env::current_dir().unwrap_or_else(|_| ".".into());
-
-    blocks.add_from(UserAtHost::default());
-    blocks.add_from(Cwd::new(&cwd));
-    blocks.add_from(Git::new(&cwd));
-    blocks.add_from(Pipenv::default());
-
+    let blocks = Blocks::from_names(env::args().skip(1));
     let prompt = format(blocks.iter());
-    // println!("{:?}", prompt);
     print!("{}", prompt);
 }
