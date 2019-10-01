b65b274561ae7df42d279b45a69e14bbd09c8a65
Philipp Joram <mail@phijor.me>
2019-10-01 20:03:48 +0200

    Git source: shorten refs, add rebase/merge indicator

--- src/git.rs
+++ src/git.rs
@@ -11,37 +11,74 @@ const STYLE: Style = Style::new()
     .with_bold();
 
 pub struct Git {
-    path: PathBuf,
+    repo_root: Option<PathBuf>,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum CurrentState {
+    Normal,
+    Rebasing,
+    Merging,
 }
 
 impl Git {
     pub fn new<P: AsRef<Path>>(path: P) -> Self {
         Self {
-            path: path.as_ref().to_path_buf(),
+            repo_root: Self::find_root(path.as_ref()),
         }
     }
 
+    fn find_root(path: &Path) -> Option<PathBuf> {
+        path.ancestors()
+            .find(|candidate| candidate.join(".git").exists())
+            .map(Path::to_path_buf)
+    }
+
+    fn root(&self) -> Option<&Path> {
+        self.repo_root.as_ref().map(|root| root.as_ref())
+    }
+
     pub fn current_brach(&self) -> Option<String> {
-        for candidate in self.path.ancestors() {
-            match read_to_string(candidate.join(".git/HEAD")) {
-                Err(_) => continue,
-                Ok(head) => {
-                    return Some(
-                        head.trim_end()
-                            .trim_start_matches("ref: refs/heads/")
-                            .to_string(),
-                    )
+        let head = read_to_string(self.root()?.join(".git/HEAD")).ok()?;
+        Some(Self::shorten(head.trim_end().trim_start_matches("ref: refs/heads/")).to_string())
+    }
+
+    fn shorten(ref_spec: &str) -> &str {
+        if ref_spec.chars().all(|c| c.is_ascii_hexdigit()) {
+            &ref_spec[..7]
+        } else {
+            ref_spec
+        }
+    }
+
+    fn current_state(&self) -> Option<CurrentState> {
+        self.root().map(|root| {
+            for (ref_file, state) in &[
+                (".git/MERGE_HEAD", CurrentState::Merging),
+                (".git/REBASE_HEAD", CurrentState::Rebasing),
+            ] {
+                if root.join(ref_file).exists() {
+                    return *state;
                 }
             }
-        }
 
-        None
+            CurrentState::Normal
+        })
     }
 }
 
 impl Source for Git {
     fn get_block(&self) -> Option<Block> {
-        self.current_brach()
-            .map(|branch| Block::new(format!(" {}", branch)).with_style(STYLE))
+        let branch = self.current_brach()?;
+        let (indicator, fg) = match self.current_state()? {
+            CurrentState::Normal => ("", BaseColor::WHITE),
+            CurrentState::Rebasing => ("↥", BaseColor::RED),
+            CurrentState::Merging => ("⥇", BaseColor::YELLOW),
+        };
+
+        Some(
+            Block::new(format!("{} {}", indicator, branch))
+                .with_style(STYLE.with_fg(Color::new(fg, Brightness::NORMAL))),
+        )
     }
 }
