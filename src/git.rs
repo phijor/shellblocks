7023807daba31a5773a38826b28bd35914a500a6
Philipp Joram <mail@phijor.me>
2025-01-10 00:14:36 +0200

    Fix git branch block not displaying inside of submodules
    The logic for finding a Git top-level directory failed for submodules
    which had a `.git` *file* present.  Instead of reinventing the wheel,
    use `git rev-parse --show-toplevel` to get the top-level directory.
    
    Similarly, use `git symbolic-ref --short HEAD` to obtain the current
    branch name, and fall back to `git rev-parse --short` if that fails.

--- src/git.rs
+++ src/git.rs
@@ -6,16 +6,18 @@ use crate::block::Block;
 use crate::source::{Context, Source};
 use crate::style::{BaseColor, Brightness, Color, Style};
 
-use std::fs::read_to_string;
-use std::path::Path;
+use std::ffi::OsString;
+use std::os::unix::ffi::OsStringExt;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
 const STYLE: Style = Style::new()
     .with_fg(Color::new(BaseColor::White, Brightness::Normal))
     .with_bg(Color::new(BaseColor::Black, Brightness::Bright))
     .with_bold();
 
-pub struct GitDir<'r> {
-    root: &'r Path,
+pub struct GitDir {
+    root: PathBuf,
 }
 
 #[derive(Default)]
@@ -28,25 +30,50 @@ enum CurrentState {
     Merging,
 }
 
-impl<'r> GitDir<'r> {
-    fn find(path: &'r Path) -> Option<Self> {
-        let root = path
-            .ancestors()
-            .find(|candidate| candidate.join(".git").exists())?;
+trait CommandReadStdout {
+    fn read_stdout(self) -> Option<Vec<u8>>;
+}
+
+impl CommandReadStdout for &mut Command {
+    fn read_stdout(self) -> Option<Vec<u8>> {
+        let output = self.output().ok()?;
+        if output.status.success() {
+            Some(output.stdout)
+        } else {
+            None
+        }
+    }
+}
+
+impl GitDir {
+    fn find(path: &Path) -> Option<Self> {
+        let output = Command::new("git")
+            .arg("rev-parse")
+            .arg("--show-toplevel")
+            .current_dir(path)
+            .read_stdout()?;
+
+        let root = PathBuf::from(OsString::from_vec(output));
         Some(Self { root })
     }
 
     pub fn current_branch(&self) -> Option<String> {
-        let head = read_to_string(self.root.join(".git/HEAD")).ok()?;
-        Some(Self::shorten(head.trim_end().trim_start_matches("ref: refs/heads/")).to_string())
-    }
-
-    fn shorten(ref_spec: &str) -> &str {
-        if ref_spec.chars().all(|c| c.is_ascii_hexdigit()) {
-            &ref_spec[..7]
-        } else {
-            ref_spec
+        fn git(args: &[&'static str]) -> Command {
+            let mut cmd = Command::new("git");
+            cmd.args(args);
+            cmd
         }
+
+        [
+            git(&["symbolic-ref", "--short", "HEAD"]),
+            git(&["rev-parse", "--short", "HEAD"]),
+        ]
+        .into_iter()
+        .find_map(|mut cmd| {
+            let output = cmd.read_stdout()?;
+            let branch = String::from_utf8(output).ok()?;
+            Some(branch.trim().into())
+        })
     }
 
     fn current_state(&self) -> CurrentState {
